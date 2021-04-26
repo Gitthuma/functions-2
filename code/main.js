64221bcc1622f0681ebf586d216e3a3c3787e684
Gitthuma <george.githuma@gmail.com>
2021-04-26 17:51:31 +0300

    Created a button with the wording 'Draw Rectangle' and appended it inside <section>

--- code/main.js
+++ code/main.js
@@ -9,6 +9,12 @@ let color = 'blue';
 
 // Add your code here
 
+/*Create a button with the wording 'Draw Rectangle' and append it inside <section>*/
+
+const btn = document.createElement('button');
+btn.textContent = 'Draw Rectangle';
+
 // Don't edit the code below here!
 
 const section = document.querySelector('section');
+section.appendChild(btn);
