c8408546d574898a0f315d1c26ea87fe3f748452
Gitthuma <george.githuma@gmail.com>
2021-04-26 12:58:42 +0300

    Added README.md, index.html, style.css and main.js with functions-2 starter code

---
+++ code/main.js
@@ -0,0 +1,14 @@
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+let x = 50;
+let y = 60;
+let width = 100;
+let height = 75;
+let color = 'blue';
+
+// Add your code here
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
